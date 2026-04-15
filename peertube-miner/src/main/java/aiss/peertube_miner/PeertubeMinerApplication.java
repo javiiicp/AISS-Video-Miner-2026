ba10e60830fdd87f699aac69d5dd7527d03ddbaa
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-15 09:57:42 +0200

    Añadida estructura SpringBoot de PeerTube

---
+++ peertube-miner/src/main/java/aiss/peertube_miner/PeertubeMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.peertube_miner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeertubeMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeertubeMinerApplication.class, args);
+	}
+
+}
