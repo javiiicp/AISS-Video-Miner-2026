abc7a12c6a26ad5404425bd1544094156419ab1d
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-15 10:15:37 +0200

    Iniciado el archivo UserRepository.java

--- videominer/src/main/java/aiss/videominer/repository/UserRepository.java
+++ videominer/src/main/java/aiss/videominer/repository/UserRepository.java
@@ -1,1 +1,11 @@
+package aiss.videominer.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import aiss.videominer.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
