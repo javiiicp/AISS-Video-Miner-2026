87bb43d9992c5470dd7e3ee2295166c03e8d3177
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-16 13:21:33 +0200

    Actualizado UserRepository

--- videominer/src/main/java/aiss/videominer/repository/UserRepository.java
+++ videominer/src/main/java/aiss/videominer/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package aiss.videominer.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import aiss.videominer.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+    // Este método para filtrar por nombre (ignora mayúsculas/minúsculas)
+    Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
