898fe211fcb2c4ec6bdcb720bd8e13cd45e64c34
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-16 13:03:29 +0200

    Creados los repositorios Comment y Caption

--- videominer/src/main/java/aiss/videominer/repository/CommentRepository.java
+++ videominer/src/main/java/aiss/videominer/repository/CommentRepository.java
@@ -1,1 +1,8 @@
+package aiss.videominer.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import aiss.videominer.model.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, String> {}
