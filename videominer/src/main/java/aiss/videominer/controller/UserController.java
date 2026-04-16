e58ef3b7733497c5d079f90fd9ab45731f17eea3
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-16 13:19:27 +0200

    Cambios menores

--- videominer/src/main/java/aiss/videominer/controller/UserController.java
+++ videominer/src/main/java/aiss/videominer/controller/UserController.java
@@ -9,8 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import aiss.videominer.model.User;
@@ -20,7 +20,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/videominer/users")
-@Tag(name = "User Controller", description = "Operaciones para gestionar los usuarios (autores de vídeos y comentarios)")
+@Tag(name = "User Controller", description = "Operaciones para gestionar los usuarios")
 public class UserController {
 
     @Autowired
