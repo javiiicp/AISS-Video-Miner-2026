27776f82a5f6f103d7d07b3106326be37970f0cd
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-16 14:19:53 +0200

    cambios menores

--- videominer/src/main/java/aiss/videominer/controller/UserController.java
+++ videominer/src/main/java/aiss/videominer/controller/UserController.java
@@ -1,7 +1,5 @@
 package aiss.videominer.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
