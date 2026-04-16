b3e8ca677d120f4d0f5be17b9441ab7ba5542eed
FIRST_NAME LAST_NAME <josadomar@alumus.es>
2026-04-16 14:31:30 +0200

    creado el controller y modificado la carpeta service de peertubeminer

--- peertube-miner/src/main/java/aiss/peertube_miner/service/PeerTubeService.java
+++ peertube-miner/src/main/java/aiss/peertube_miner/service/PeerTubeService.java
@@ -1,10 +1,13 @@
 package aiss.peertube_miner.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import aiss.peertube_miner.model.Channel;
+import aiss.peertube_miner.model.Video;
 import aiss.peertube_miner.model.VideoSearchResponse;
 
 @Service
@@ -13,21 +16,34 @@ public class PeerTubeService {
     @Autowired
     RestTemplate restTemplate;
 
-    public Channel getChannel(String instance, String channelId, int maxVideos) {
-        // 1. URL del canal
-        String urlCanal = "https://" + instance + "/api/v1/video-channels/" + channelId;
-        
-        // 2. Obtener el canal (Igual que en clase)
+    public Channel getChannel(String instance, String channelId, int maxVideos, int maxComments) {
+        String baseUrl = "https://" + instance + "/api/v1";
+
+        // 1. Obtener el Canal
+        String urlCanal = baseUrl + "/video-channels/" + channelId;
         Channel canal = restTemplate.getForObject(urlCanal, Channel.class);
 
         if (canal != null) {
-            // 3. URL de los vídeos
+            // 2. Obtener los Vídeos (usando maxVideos)
             String urlVideos = urlCanal + "/videos?count=" + maxVideos;
-            
-            // 4. Obtener la respuesta de vídeos y extraer la lista
-            VideoSearchResponse res = restTemplate.getForObject(urlVideos, VideoSearchResponse.class);
-            if (res != null) {
-                canal.setVideos(res.getData());
+            VideoSearchResponse resVideos = restTemplate.getForObject(urlVideos, VideoSearchResponse.class);
+
+            if (resVideos != null && resVideos.getData() != null) {
+                List<Video> videos = resVideos.getData();
+
+                // 3. PARA CADA VÍDEO -> Buscar sus comentarios
+                for (Video video : videos) {
+                    // URL de hilos de comentarios del vídeo (usando maxComments)
+                    String urlComments = baseUrl + "/videos/" + video.getId() + "/comment-threads?count=" + maxComments;
+                    
+                    CommentResponse resComments = restTemplate.getForObject(urlComments, CommentResponse.class);
+                    
+                    if (resComments != null) {
+                        video.setComments(resComments.getData());
+                    }
+                }
+                // Guardamos la lista de vídeos ya "completados" en el canal
+                canal.setVideos(videos);
             }
         }
         return canal;
