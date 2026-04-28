2c29338c8086f7829a16bbeb69b4fed01d4e24e4
José Antonio Adorna Martínez <josadomar@alumus.es>
2026-04-28 14:16:21 +0200

    cambio en peertubeminerapplication

--- peertube-miner/src/main/java/aiss/peertube_miner/model/VideoSearchResponse.java
+++ peertube-miner/src/main/java/aiss/peertube_miner/model/VideoSearchResponse.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class VideoSearchResponse {
     private List<Video> data; // PeerTube envía los vídeos aquí
-    public List<Video> getData() { return data; }
-    public void setData(List<Video> data) { this.data = data; }
+
+    public List<Video> getData() { 
+        return data; 
+    }
+    
+    public void setData(List<Video> data) { 
+        this.data = data; 
+    }
 }
