dd7be5b3978f67512fd3e78c69f499f7ecbd1c36
Educrurom <educrurom@alum.us.es>
2026-04-29 18:04:41 +0200

    fix: agregar imports necesarios en DailymotionMinerApplication

--- dailymotion-miner/src/main/java/aiss/dailymotion_miner/DailymotionMinerApplication.java
+++ dailymotion-miner/src/main/java/aiss/dailymotion_miner/DailymotionMinerApplication.java
@@ -2,6 +2,9 @@ package aiss.dailymotion_miner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DailymotionMinerApplication {
