8912759d1b7f8943befd7cb26251ae81c2e326c9
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-04-29 10:20:14 +0200

    Mejorado el UserController para el manejo de errores

--- peertube-miner/src/main/java/aiss/peertube_miner/controller/UserController.java
+++ peertube-miner/src/main/java/aiss/peertube_miner/controller/UserController.java
@@ -4,9 +4,15 @@ import aiss.peertube_miner.model.User;
 import aiss.peertube_miner.model.external.ApiAccount;
 import aiss.peertube_miner.service.ApiUserService;
 import aiss.peertube_miner.service.VideoMinerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import aiss.peertube_miner.exception.UserNotFoundException;
 
 @RestController
 @RequestMapping("/peertubeminer/users")
@@ -22,17 +28,23 @@ public class UserController {
     RestTemplate restTemplate;
 
     @PostMapping("/{instance}/{username}")
+    @ResponseStatus(HttpStatus.CREATED) // Opcional: devuelve 201 si todo va bien
     public void mineUser(@PathVariable String instance, @PathVariable String username) {
-        // 1. Buscamos en PeerTube
-        String url = "https://" + instance + "/api/v1/accounts/" + username;
-        ApiAccount externalAccount = restTemplate.getForObject(url, ApiAccount.class);
+        try {
+            // 1. Intentamos buscar en PeerTube
+            String url = "https://" + instance + "/api/v1/accounts/" + username;
+            ApiAccount externalAccount = restTemplate.getForObject(url, ApiAccount.class);
 
-        // 2. Traducimos a vuestro modelo User usando TU servicio
-        User user = apiUserService.transformToUser(externalAccount);
-
-        // 3. Enviamos a VideoMiner para que lo guarde
-        if (user != null) {
+            // 2. Traducimos y enviamos (Tu parte de User)
+            User user = apiUserService.transformToUser(externalAccount);
             videoMinerService.saveUser(user);
+
+        } catch (HttpClientErrorException.NotFound e) {
+            // Si PeerTube dice 404, lanzamos nuestra excepción de 404
+            throw new UserNotFoundException();
+        } catch (RestClientException e) {
+            // Para cualquier otro error, que siga saliendo el 500
+            throw e;
         }
     }
 }
