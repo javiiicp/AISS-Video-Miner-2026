a83055223d3cf4ca22dfefd94066bcad035413e9
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-05-05 12:19:09 +0200

    Actualizado Swagger OAS en VideoMiner

--- dailymotion-miner/src/main/java/aiss/dailymotion_miner/service/VideominerService.java
+++ dailymotion-miner/src/main/java/aiss/dailymotion_miner/service/VideominerService.java
@@ -2,10 +2,8 @@ package aiss.dailymotion_miner.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import aiss.dailymotion_miner.model.Channel;
 
 @Service
@@ -16,10 +14,9 @@ public class VideominerService {
 
     @Value("${VIDEOMINER_BASE_URL:http://localhost:8080}")
     private String videominerBaseUrl;
-
-    public Channel saveChannel(Channel channel) {
+    
+    public void saveChannel(Channel channel) {
         String url = videominerBaseUrl + "/videominer/channels";
-        ResponseEntity<Channel> resp = restTemplate.postForEntity(url, channel, Channel.class);
-        return resp.getBody();
+        restTemplate.postForObject(url, channel, Channel.class);
     }
 }
