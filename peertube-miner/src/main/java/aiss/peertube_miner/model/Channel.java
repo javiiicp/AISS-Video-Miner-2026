66d2a63ea24d6a6b6ecc6ed653210002c2f78158
JAVIER CAMPOS DE LA PEÑA <javcamde@alum.us.es>
2026-05-07 11:57:49 +0200

    UUID de peertube

--- peertube-miner/src/main/java/aiss/peertube_miner/model/Channel.java
+++ peertube-miner/src/main/java/aiss/peertube_miner/model/Channel.java
@@ -2,24 +2,30 @@ package aiss.peertube_miner.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Datos del canal de PeerTube recolectados")
 public class Channel {
 
     @JsonProperty("id")
+    @Schema(description = "ID interno del canal (proporcionado por PeerTube)", example = "123")
     private String id;
 
     @JsonProperty("name")
+    @Schema(description = "Nombre corto o 'handle' del canal utilizado para las búsquedas", example = "aiss_channel")
     private String name;
 
     @JsonProperty("description")
+    @Schema(description = "Descripción biográfica del canal")
     private String description;
 
     @JsonProperty("createdTime")
+    @Schema(description = "Fecha de creación del canal en formato ISO 8601", example = "2024-05-20T10:00:00Z")
     private String createdTime;
 
     @JsonProperty("videos")
+    @Schema(description = "Lista de vídeos pertenecientes a este canal")
     private List<Video> videos;
 
     public Channel() {
--- peertube-miner/src/main/java/aiss/peertube_miner/model/User.java
+++ peertube-miner/src/main/java/aiss/peertube_miner/model/User.java
@@ -1,52 +1,35 @@
 package aiss.peertube_miner.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Información del autor en PeerTube")
 public class User {
 
     @JsonProperty("id")
+    @Schema(description = "ID del usuario", example = "10")
     private String id;
 
     @JsonProperty("name")
+    @Schema(description = "Nombre visible del autor", example = "AISS User")
     private String name;
 
     @JsonProperty("user_link")
+    @Schema(description = "URL del perfil del autor en la plataforma")
     private String user_link;
 
     @JsonProperty("picture_link")
+    @Schema(description = "URL del avatar del autor")
     private String picture_link;
 
     public User() {}
 
-    public String getId() { 
-        return id; 
-        }
-
-    public void setId(String id) { 
-        this.id = id; 
-        }
-
-    public String getName() { 
-        return name; 
-        }
-
-    public void setName(String name) { 
-        this.name = name; 
-        }
-
-    public String getUser_link() { 
-        return user_link; 
-        }
-
-    public void setUser_link(String user_link) { 
-        this.user_link = user_link; 
-        }
-
-    public String getPicture_link() { 
-        return picture_link; 
-        }
-
-    public void setPicture_link(String picture_link) { 
-        this.picture_link = picture_link; 
-        }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getUser_link() { return user_link; }
+    public void setUser_link(String user_link) { this.user_link = user_link; }
+    public String getPicture_link() { return picture_link; }
+    public void setPicture_link(String picture_link) { this.picture_link = picture_link; }
 }
